bf4c3f71107350c07004389fd11b328c662e025d
Xiddler <xiddler@gmail.com>
2018-07-06 15:03:25 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # basic_flask
+2018-07-06_15:59 I added this in the aws cli  
 this is the flask app that contains the script for lanner - which allows snippets to be moved from a to b
---
+++ create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE snippets (
+    id integer PRIMARY KEY NOT NULL,
+    snippet text NOT NULL
+);
+
+INSERT INTO  snippets (snippet) values ('erjlklko45djdgkl /gdjgdfkflk');
